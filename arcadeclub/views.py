d280186dca79a5f95bf34d1e44bd8d31df95d3fa
matteo <matteo.capparrucci@gmail.com>
2016-05-18 12:11:47 +0200

    test

--- arcadeclub/views.py
+++ arcadeclub/views.py
@@ -148,6 +148,7 @@ def magazzino_detail(request):
 
 def venduti_detail(request):
     if request.method == 'PUT':
+        print request.body()
         id_item  = request.PUT.get("id_item",'ERROR')
         prezzo  = request.PUT.get("prezzo",'')
         data  = request.PUT.get("data",'')
