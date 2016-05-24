8c99e525e512868dc2ba5e88ad428009f763ffd4
matteo <matteo.capparrucci@gmail.com>
2016-05-24 10:48:04 +0200

    nuovo script sito funzionante

--- search_upc/requestSitoNew.py
+++ search_upc/requestSitoNew.py
@@ -15,6 +15,28 @@ def __webSearchNew(upc):
         sito = "http://videogames.pricecharting.com/search?q=" + upc
         r = http.request('GET', sito)
 
-        print r.data
+        temp = r.data
+        page = temp
+
+        temp = temp.split("Prices")
+
+        titolo = temp[0].split("<title>")[1]
+
+        console = temp[1].split(" | Compare Loose")[0]
+
+        console = console.replace("(", "").replace(")", "").replace(" ", "")
+
+        anno = (page.split("class=\"date\">")[1]).split("<")[0]
+
+        foto = page.split("<img src=\"")[1].split("\"")[0]
+
+
     except urllib3.exceptions.SSLError as e:
         print e
+
+    print "nome:", titolo
+    print "anno:", anno
+    print "immagine:", foto
+    print "console:", console
+
+    return {'nome':titolo, 'anno':anno, 'immagine':foto,'console':console,'upc':upc}
