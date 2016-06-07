4c420d2b2acf1cb711478288d2780905ff1fabe9
matteo <matteo.capparrucci@gmail.com>
2016-06-07 18:21:36 +0200

    prove sul db

--- arcadeclub/views.py
+++ arcadeclub/views.py
@@ -216,6 +216,7 @@ def searchUpcRequest(request,id_telefono):
         if request.method == 'GET':
             try:
                 upc  = request.GET.get("upc",'no_upc')
+                print upc
                 gioco = Gioco.objects.get(upc=upc)
             except Gioco.DoesNotExist:
                 #datiGioco = __webSearch(upc)
